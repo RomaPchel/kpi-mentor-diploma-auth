bfe647154298339ddd9174410c9cd8ad1415000b
romapchel <notpchelintsevroma@gmail.com>
2025-01-24 00:07:53 +0200

    fixed paths to entities

--- app/src/db/config.ts
+++ app/src/db/config.ts
@@ -4,12 +4,8 @@ import { EntityManager } from "@mikro-orm/postgresql";
 
 export const orm = await MikroORM.init({
   metadataProvider: TsMorphMetadataProvider,
-  entities: [
-    "/Users/romapchel/KPI/kpi-mentor-diploma-auth/app/dist/entities/*.js",
-  ],
-  entitiesTs: [
-    "/Users/romapchel/KPI/kpi-mentor-diploma-auth/app/src/entities/*.ts",
-  ],
+  entities: ["./dist/entities/*.js"],
+  entitiesTs: ["./src/entities/*.ts"],
   dbName: "diploma",
   host: "localhost",
   port: 5432,
