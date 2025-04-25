ff820ab887e0343bc7c541bee2a362f17064b35b
romapchel <notpchelintsevroma@gmail.com>
2025-04-25 17:15:01 +0300

    tests

---
+++ app/tests/test.ts
@@ -0,0 +1,7 @@
+describe("Calculator Functions", () => {
+  describe("divide", () => {
+    it("should divide two positive numbers correctly", () => {
+      expect(6 / 2).toBe(3);
+    });
+  });
+});
