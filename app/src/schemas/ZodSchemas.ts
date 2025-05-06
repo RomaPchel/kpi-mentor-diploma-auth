2e44009b714ef1f06fb2dce0ac55f8a68ac64cfa
MarkRufin-Zavhorodni <Mark.Rufin-Zavhorodnii@nangasystems.com>
2025-05-06 21:41:09 +0300

    f

--- app/src/schemas/ZodSchemas.ts
+++ app/src/schemas/ZodSchemas.ts
@@ -16,20 +16,6 @@ export const LoginRequestSchema = z.object({
     .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
-export const BecomeMentorRequestSchema = z.object({
-  motivation: z
-    .string()
-    .min(32, { message: "Motivation should be at least 32 characters long" }),
-});
-
-export const UUIDParamSchema = z.object({
-  chatId: z.string().uuid({ message: "Invalid chat ID format" }),
-});
-
-export const MentorRequestParamSchema = z.object({
-  id: z.string().uuid({ message: "Invalid request ID format" }),
-});
-
 export const CreateEventSchema = z.object({
   url: z
     .string()
